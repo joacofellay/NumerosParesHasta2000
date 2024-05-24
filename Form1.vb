2e6bad19a612a9fb487e66bc03842d9ba18cbb54
Joaquin Fellay <joafellay@gmail.com>
2024-05-24 13:17:03 -0300

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function GetTxtLabel1() As Label
+        Return txtLabel1
+    End Function
+
+    Private Sub txtBoton_Click(sender As Object, e As EventArgs) Handles txtBoton.Click
+        For N = 2 To 2000 Step 2
+        Next
+    End Sub
+End Class
